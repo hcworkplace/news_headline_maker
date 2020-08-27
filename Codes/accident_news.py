c92d1896f077ac33415a7d4b124260d0b03b53a1
rom_msi <kim0br0309@gmail.com>
2020-08-28 02:56:34 +0900

    크롤링 코드 수정 및 데이터 업로드

--- Codes/accident_news.py
+++ Codes/accident_news.py
@@ -4,8 +4,8 @@ import csv
 
 soup_objects = []
 
-for d in range(20200817,20200818):
-    for p in range(1,31):
+for d in range(20200817,20200827):
+    for p in range(1, 41):
         list_url = f'https://news.naver.com/main/list.nhn?mode=LS2D&sid2=249&mid=shm&sid1=102&date={d}&page={p}'
 
         response = requests.get(list_url)
@@ -39,12 +39,17 @@ for soup in soup_objects:
         soup_content = BeautifulSoup(each_news.text, 'html.parser')
         content = soup_content.select_one('#articleBodyContents')
         # print('content : ' + content.text)
-        news_data_schema['news_content'] = content.text
-        print(news_data_schema, '\n')
+        try:
+            text = content.text.replace('\n', '').replace('\t', '').replace('// flash 오류를 우회하기 위한 함수 추가function _flash_removeCallback() {}', '')
+            text = text.split('▶')[0]
+            news_data_schema['news_content'] = text
+        except AttributeError:
+            news_data_schema['news_content'] = ''
+        # print(news_data_schema, '\n')
 
-        # with open('./naver_accident_news.csv', 'a') as csvfile:
-        #     fieldnames = ['news_title', 'news_content']
-        #     csv_writer = csv.DicWriter(csvfile, fieldnames=fieldnames)
-        #     csv_writer.writerow(news_data_schema)
+        with open('../Data/naver_news_accident.csv', 'a', encoding='utf-8') as csvfile:
+            fieldnames = ['news_title', 'news_content']
+            csv_writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            csv_writer.writerow(news_data_schema)
 
-        #     print(news_data_schema, '\n')
+            print(news_data_schema, '\n')
